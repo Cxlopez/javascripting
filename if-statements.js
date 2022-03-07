345b6b03a35c942fdafa9306a4c76d049dcc6ad1
Cxlopez <cristian4402@hotmail.com>
2022-03-06 22:22:40 -0500

    funky

---
+++ if-statements.js
@@ -0,0 +1,7 @@
+let fruit = 'orange';
+if(i >= fruit.length){
+console.log("The fruit has more than five characters.")
+} else {
+  console.log("The fruit has five characters or less.")
+}
+console.log(i);
---
+++ round-numbers.js
@@ -0,0 +1,4 @@
+const roundUp = 1.5
+Math.round(1.5);
+let rounded = math.round(1.5);
+console.log(rounded);
